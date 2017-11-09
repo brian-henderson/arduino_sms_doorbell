dab160c7a0c9380adaf8f7fca134b9bb8cbf3294
brian-henderson <brian.henderson1@marist.edu>
2017-11-09 00:00:01 -0500

    confirm button now displays user number, placeholder for running script to send sms

--- arduino_src/arduino_src.ino
+++ arduino_src/arduino_src.ino
@@ -5,38 +5,37 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int toggleBtnPin = 9;
 int confirmBtnPin = 10;
-char* names[] = {"Brian Henderson","Zach Toner","Drew Burns","Tanner Senius","Jon Coogan"};
+char* contactNames [] = {"Brian Henderson","Zach Toner","Drew Burns","Tanner Senius","Jon Coogan"};
+char* contactNumbers[] = {"7323433511", "5165927041", "5185066268", "8452690469", "5187286461"};
 int currIndex = 0;
-int arrSize = sizeof(names)/sizeof(names[0]);
+int arrSize = sizeof(contactNames)/sizeof(contactNames[0]);
 
 
 void setup() {
-  // set button pins
+  // set button pins and type
   pinMode(toggleBtnPin, INPUT);
   pinMode(confirmBtnPin, INPUT);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print(names[currIndex]);
+  lcd.print(contactNames[currIndex]);
 }
 
 void loop() {
   // set the cursor to column 0, line 1
   if(digitalRead(toggleBtnPin) == HIGH) {
+    lcd.clear();
     currIndex ++;
     if(currIndex == arrSize) {
       currIndex = 0;
     }
-    lcd.clear();
-    // set the cursor to column 0, line 1
     lcd.setCursor(0, 0);
-    lcd.print(names[currIndex]);
-    // set cursor to coloumn 0, line 2
-    lcd.setCursor(0, 2);
-    delay(100);
+    lcd.print(contactNames[currIndex]);
+    delay(200);
   }
 
   if(digitalRead(confirmBtnPin) == HIGH){
-    Serial.print(names[currIndex]);
+    lcd.setCursor(0, 1);
+    lcd.print(contactNumbers[currIndex]);
   }
 }
