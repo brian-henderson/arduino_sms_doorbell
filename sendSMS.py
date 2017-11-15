a42cc3d58a483781c46909754d6ba834a40e1110
Brian Henderson <brian.henderson1@marist.edu>
2017-11-15 17:41:20 -0500

    Added serial code to py script

--- sendSMS.py
+++ sendSMS.py
@@ -1,4 +1,7 @@
+import serial
 from twilio.rest import Client
+
+ser = serial.Serial('COM4', 9600)
 
 # Account ID
 account_sid = "AC2c499a37e76bd87fb370afc8d6e57be7"
@@ -7,9 +10,15 @@ auth_token  = "#################################"
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-	to="+17323433511", 
-	from_="+12019480413",
-	body="Python Script Test")
+def sendText(reciever):
+	message = client.messages.create(
+		to=reciever, 
+		from_="+12019480413",
+		body="Someone is at the front door for you!")
+	print(message.sid)
 
-print(message.sid)
+while True:
+	console = ser.readline()
+	if console[0] == "+":
+		sendText(console)
+		print("Text sent")
