268ab01e8cbb5aac1193bbaa3d1484fd83cd7d63
Brian Henderson <brian.henderson1@marist.edu>
2017-11-30 10:34:45 -0500

    Added resources

--- sendSMS.py
+++ sendSMS.py
@@ -7,7 +7,7 @@ ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
 account_sid = "AC2c499a37e76bd87fb370afc8d6e57be7"
 
 # Your Auth Token from twilio.com/console
-auth_token  = "c88ab2d9be99aa0a631f12d7c768fba6"
+auth_token  = ####################################
 
 client = Client(account_sid, auth_token)
 
@@ -16,10 +16,9 @@ def sendText(reciever):
 		to=reciever,
 		from_="+12019480413",
 		body="Someone is at the front door for you!")
-	print(message.sid)
+
 
 while True:
 	console = ser.readline()
 	if console[0] == "+":
 		sendText(console)
-		print("Text sent")
